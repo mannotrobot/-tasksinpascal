ec35dabcdf6cf8db006d2d224816f963916c8b1d
mannotrobot <boxforgit@gmail.com>
2023-07-30 21:07:21 +0300

    задачи 33 - 37 выполнены

--- exp33.pas
+++ exp33.pas
@@ -10,8 +10,15 @@ ok    - при пересечение границы экрана перемеж
 ok    - escape выход;
 ok    - направление движения меняется под прямым углом;
 ok    - изменение направления при нажатии клавиш;
-      - добавить очередь для изменения направления, если \
-        очередб пуста менять радномно;
+ok    - добавить очередь для изменения направления, если \
+        очередь пуста менять радномно;
+ok    - ограничить число перемещения 1000;
+ok    - ограничить число ходов 10;
+ok    - разместить в центре квадрат 3*3;
+ok    - припопадание  * в квадрат, закончить игру.
+ok    - вывести на экран YOU WIN;
+ok    - в случаи оканчания ходов или перемещения закончить игру;
+ok    - вывести на экран GAME OVER; 
 }
 
 uses
@@ -45,8 +52,12 @@ type
         state : boolean;
         count : integer;
         pos   : integer;
-        dirs  : array [0..999] of integer;
+        dirs  : array [0..9] of integer;
     end;
+
+    coord = record
+        x, y : integer;
+    end; 
 
 procedure show_object(var obj: object_game);
 begin
@@ -147,43 +158,121 @@ var
     cur: integer;
 begin
     cur := queue.count;
-    queue.count := queue.count + 1;
-    if queue.count + 1 > 999 then
+    if queue.count + 1 > 10 then
     begin
-        queue.state := False;
+        if queue.pos > 9 then
+        begin
+            queue.state := False;
+        end
+        else
+        begin
+            queue.state := True;
+        end;
     end
     else
     begin
         queue.dirs[cur] := delta;
         queue.state := True;
+        queue.count := queue.count + 1;
     end;
+end;
+
+procedure show_cube(var cube: object_game; var cubec: array of coord);
+var
+    i  : integer;
+    j  : integer;
+    obj: object_game;
+    pos: integer;
+begin
+    pos := 0;
+    obj := cube;
+    for i:= 0 to 2 do
+    begin
+        for j:= 0 to 2 do
+        begin
+            cubec[pos].x := cube.x;
+            cubec[pos].y := cube.y;
+            show_object(cube);
+            cube.x := cube.x + 1;
+            pos := pos + 1; 
+        end;
+        cube.y := cube.y + 1;
+        cube.x := cube.x - 3; 
+    end;
+    cube:= obj;
+end;
+
+function check(var obj: object_game;var cubec: array of coord ): boolean;
+var
+    cur  : coord;
+    flag : boolean;
+begin
+    flag := False;
+    for cur in cubec do
+    begin
+        if (cur.x = obj.x) and (cur.y = obj.y) then
+            flag := True;
+    end;
+    check := flag;
 end;
 
 var
     star : object_game;
     code : integer;
     dirs : queue_dir;
+    count: integer;
+    flag : boolean;
+    win  : boolean;
+    cube : object_game;
+    cubec: array [0..9] of coord;
 begin
     clrscr;
     randomize;
-    star.x := screenwidth div 2;
-    star.y := screenheight div 2;
+    count := 0;
+    cube.x := screenwidth div 2 - 2;
+    cube.y := screenheight div 2 - 2;
+    cube.symbol := '@';  
+    star.x := 2;
+    star.y := 2;
     star.symbol := '*';
     star.count := 0;
     star.direction := 1;
-    while True do
+    flag := True;
+    win := False;
+    dirs.state := False;
+    show_cube(cube, cubec);
+    while flag do
     begin
        while not keypressed do
        begin
+           if (count > 1000) or ((dirs.pos > 9) and dirs.state)  then
+           begin
+               flag := false;
+               break;
+           end;
+           if check(star, cubec) then
+           begin
+               win := True;
+               flag := False;
+               break;
+           end;
            gotoxy(1, 1);
-           write(star.count);
-           write(' | ходов осталось [ ',999 - dirs.count ,' ]');
+           write('pos ', dirs.pos, ' ');
+           write('шаги: ',1000 - count, ' ');
+           write('ходы: ',10 - dirs.count, ' ');
+           write('xy cube: ', cube.x , '|', cube.y, ' ');
+           write('xy star: ', star.x, '|', star.y, ' ');
            gotoxy(1, 1);
            random_move(star, dirs);
+           count := count + 1;
            if dirs.count = dirs.pos then
            begin
                dirs.state := False;
            end;
+       end;
+       if not flag then
+       begin
+           break;
        end;
        get_key(code);
        case code of
@@ -193,6 +282,22 @@ begin
            -72: add_dir(dirs, 2);
            -80: add_dir(dirs, 3);
        end
+    end;
+    if win then
+    begin
+        clrscr;
+        gotoxy(screenwidth div 2 - 7, screenheight div 2);
+        textcolor(blue);
+        write('YOU WIN');
+        delay(3000);
+    end
+    else
+    begin
+        clrscr;
+        gotoxy(screenwidth div 2 - 8, screenheight div 2);
+        textcolor(red);
+        write('GAME OVER');
+        delay(3000);
     end;
     clrscr;
 end.
