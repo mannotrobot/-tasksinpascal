47900e144236f6bba1114236bcbff459e91fd64c
mannotrobot <boxforgit@gmail.com>
2023-08-01 21:32:49 +0300

    complect task 246

--- exp46.pas
+++ exp46.pas
@@ -1,15 +1,31 @@
 program exp46;
 {
   задача:
-    - получить аргумент командной строки;
-    - создать файл с именем аргумента;
-    - записать в файл текст;
+ok  - получить аргумент командной строки;
+ok  - создать файл с именем аргумента;
+ok  - записать в файл текст;
 }
 const
     humpty =  'Humpty-Dumpty sat on a wall,' + #10 + 
               'Humpty-Dumpty had a great fall;'+ #10 +
               'All the kings horses, and all the kings men' + #10 +
-              'Cannot put Humpty-Dumpty together again.';
+              'Cannot put Humpty-Dumpty together again.'+#10;
+
+var
+    namefile : string;
+    f        : text;
 begin
-    writeln(humpty);
+    if paramcount = 1 then
+    begin
+        namefile := paramstr(1)+'.txt';
+        assign(f, namefile);
+        rewrite(f);
+        write(f, humpty);
+        close(f);
+    end
+    else
+    begin
+        writeln('sorry...write line name');
+        writeln(paramcount);
+    end;
 end.
